776280e47f5f8d7e03c4ee5b8907d956179ad1f6
Maryam Arif <maryamarif1313@gmail.com>
2025-11-23 00:56:18 +0500

    v1

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import Image from 'next/image';
 import logo from '../../../public/images/logo.png';
